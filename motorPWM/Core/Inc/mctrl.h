a7d40b63d7855ae2e7974a8bd7c0a0a60ca103e8
thihaElec <thiha.sales@gmail.com>
2023-11-23 22:50:16 +0800

    ST32 Nucleo 401RE stepper driver
    ST32 Nucleo401RE MicrostepDriver

---
+++ motorPWM/Core/Inc/mctrl.h
@@ -0,0 +1,16 @@
+/*
+ * mctrl.h
+ *
+ *  Created on: Nov 19, 2023
+ *      Author: kzw
+ */
+
+#ifndef INC_MCTRL_H_
+#define INC_MCTRL_H_
+
+#define Motor_Control_Port GPIOB
+#define EN_Pin GPIO_PIN_3
+#define DIR_Pin GPIO_PIN_4
+#define PUL_Pin GPIO_PIN_5
+
+#endif /* INC_MCTRL_H_ */
